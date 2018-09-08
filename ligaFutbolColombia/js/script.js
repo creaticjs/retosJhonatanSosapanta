04890548dc4f5219d200ff04ebff09c586cd3992
jasa1704 <jasosapanta@gmail.com>
2018-09-07 19:48:24 -0500

    Primeros retos resueltos

---
+++ ligaFutbolColombia/js/script.js
@@ -0,0 +1,269 @@
+var dataEquipos = [
+    {
+        nombre: "La Equidad",
+        urlimagen: "https://upload.wikimedia.org/wikipedia/commons/0/09/La_Equidad.png",
+        dt: "Luis Fernando Suárez",
+        jugadores: ["N. Barona, P.Lima, O Bernal, F. Nájera, S. Motta, J.Mahecha"],
+        resena: "El Club Deportivo La Equidad, cuya razón social es Club Deportivo La Equidad Seguros Sociedad Anónima, ​ también conocido simplemente como La Equidad es un club de fútbol de la ciudad de Bogotá, Colombia.",
+        estadio: "Metropolitano de Techo",
+        estrellas: "0",
+        categoria: "A",
+        pj:"8",
+        gf:"+11",
+        gc:"+1",
+        pts:"22",
+
+    },
+    {
+        nombre: "Once Caldas S. A.",
+        urlimagen: "https://upload.wikimedia.org/wikipedia/commons/5/52/Escudo_Oficial_Once_Caldas.png",
+        dt: "Hubert Bodhert",
+        jugadores: ["S. Román, K. Londoño, E. Velasco, J. Cuadrado, M. Restrepo, C. Amaya"],
+        resena: "La Corporación Deportiva Once Caldas S.A., más conocida simplemente como el Once Caldas, ​ es un club de fútbol de Colombia, tiene su sede en la ciudad de Manizales, departamento de Caldas, fue refundado el 15 de ... ",
+        estadio: "Palogrande",
+        estrellas: "4",
+        categoria: "A",
+        pj:"8",
+        gf:"+16",
+        gc:"+7",
+        pts:"20",
+    },
+    {
+        nombre: "Club Deportes Tolima",
+        urlimagen: "https://seeklogo.com/images/D/Deportes_Tolima-logo-41FF63827C-seeklogo.com.png",
+        dt: "Alberto Gamero",
+        jugadores: ["R. Carrascal, R. Robayo, C. Robles, J. Ramos,J. Arboleda, L. Payares"],
+        resena: "La Corporación Deportiva Once Caldas S.A., más conocida simplemente como el Once Caldas, ​ es un club de fútbol de Colombia, tiene su sede en la ciudad de Manizales, departamento de Caldas, fue refundado el 15 de ... ",
+        estadio: "Manuel Murillo Toro",
+        estrellas: "0",
+        categoria: "A",
+        pj:"8",
+        gf:"+15",
+        gc:"+8",
+        pts:"19",
+    },
+    {
+        nombre: "Atletico Junio",
+        urlimagen: "https://upload.wikimedia.org/wikipedia/commons/0/0b/Logo_de_Atl_Junior.png",
+        dt: "Julio Comesaña",
+        jugadores: ["F. Sambueza, R. Pérez, J. Gómez, S. Hernández,L. Narváez, T. Gutiérrez"],
+        resena: "El Club Deportivo Popular Junior F. C. S. A., mejor conocido como Junior de Barranquilla o también por su antiguo nombre de Atlético Junior es un club de fútbol de la ciudad de Barranquilla, Colombia.",
+        estadio: "Metropolitano Roberto Meléndez",
+        estrellas: "7",
+        categoria: "A",
+        pj:"8",
+        gf:"+12",
+        gc:"+4",
+        pts:"16",
+    },
+    {
+        nombre: "Alianza Petrolera",
+        urlimagen: "https://upload.wikimedia.org/wikipedia/commons/5/53/Logo_Oficial_Alianza_Petrolera.png",
+        dt: "Juan Cruz Real",
+        jugadores: ["P. Grazziani, L. Saldaña, C. Arias, R. Torres,P. Vranjicán, C. Pérez"],
+        resena: "La Alianza Petrolera es un club de fútbol de Colombia, originario del municipio de Barrancabermeja, en el departamento de Santander, fundado el 24 de octubre de 1991. El club debutó profesionalmente en la Categoría Primera B en la temporada 1992.",
+        estadio: "Daniel Villa Zapata",
+        estrellas: "7",
+        categoria: "A",
+        pj:"8",
+        gf:"+15",
+        gc:"+17",
+        pts:"13",
+    },
+    {
+        nombre: "Atletico Nacional",
+        urlimagen: "https://seeklogo.com/images/C/Club_Atl__tico_Nacional-logo-B34D1C41B9-seeklogo.com.png",
+        dt: "Hernan Dario Herrera",
+        jugadores: ["A. Ramírez", "C. Mafla", "D. Moreno", "G. Castellani", "F. Monetti","C. Rivas"],
+        resena: "El Atlético Nacional S. A., ​ mejor conocido como Atlético Nacional, o simplemente Nacional, es un club de fútbol de la ciudad de Medellín, Colombia, fundado el 7 de marzo de 1947 bajo el nombre ...",
+        estadio: "Atanasio Girardot",
+        estrellas: "15",
+        categoria: "A",
+        pj:"8",
+        gf:"+12",
+        gc:"+9",
+        pts:"12",
+    },
+    {
+        nombre: "Independiente Medellin",
+        urlimagen: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c2/Escudo_del_Deportivo_Independiente_Medell%C3%ADn.png/148px-Escudo_del_Deportivo_Independiente_Medell%C3%ADn.png",
+        dt: "Octavio Zambrano",
+        jugadores: ["German Cano", "David Gonzales", "Hernan Pertuz", "Ever Valencia", "Javier Calle"],
+        resena: "El Deportivo Independiente Medellín, más conocido como Independiente Medellín, DIM, o simplemente Medellín, es un club de fútbol colombiano fundado bajo el nombre de «Medellín Football Club» el 14 de noviembre de 1913 por Alberto Uribe Piedrahíta.",
+        estadio: "Atanasio Girardot",
+        estrellas: "5",
+        categoria: "A",
+        pj:"8",
+        gf:"+10",
+        gc:"+8",
+        pts:"12",
+    },
+    {
+        nombre: "Aguilas doradas",
+        urlimagen: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTd9lSb4gIYd4bFVAlnTZYw3LryWdeekOrr5lewpES-8ythA1Ve",
+        dt: "Jorge Luis Bernal",
+        jugadores: ["L. Hurtado", "J. Pérez", "L. Velázquez", "F. Rodríguez", "J. Valencia", "E. Toloza"],
+        resena: "Rionegro Águilas Doradas​ es un club de fútbol colombiano que se ubica en el municipio de Rionegro, Antioquia. Juega en la Categoría Primera A luego de obtener el título de la Primera B en 2010.",
+        estadio: "Alberto Grisales",
+        estrellas: "0",
+        categoria: "A",
+        pj:"8",
+        gf:"+8",
+        gc:"+7",
+        pts:"12",
+    },
+    {
+        nombre: "Cúcuta Deportivo",
+        urlimagen: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Escudo_del_C%C3%BAcuta_Deportivo.svg/1024px-Escudo_del_C%C3%BAcuta_Deportivo.svg.png",
+        dt: "Jorge Luis Bernal",
+        jugadores: ["L. Hurtado", "J. Pérez", "L. Velázquez", "F. Rodríguez", "J. Valencia", "E. Toloza"],
+        resena: "Rionegro Águilas Doradas​ es un club de fútbol colombiano que se ubica en el municipio de Rionegro, Antioquia. Juega en la Categoría Primera A luego de obtener el título de la Primera B en 2010.",
+        estadio: "Alberto Grisales",
+        estrellas: "0",
+        categoria: "B",
+        pj:"8",
+        gf:"+8",
+        gc:"+7",
+        pts:"12",
+    },
+    {
+        nombre: "Unión Magdalena",
+        urlimagen: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/Escudo_del_Uni%C3%B3n_Magdalena_F%C3%BAtbol_Club.png/154px-Escudo_del_Uni%C3%B3n_Magdalena_F%C3%BAtbol_Club.png",
+        dt: "Jorge Luis Bernal",
+        jugadores: ["L. Hurtado", "J. Pérez", "L. Velázquez", "F. Rodríguez", "J. Valencia", "E. Toloza"],
+        resena: "Rionegro Águilas Doradas​ es un club de fútbol colombiano que se ubica en el municipio de Rionegro, Antioquia. Juega en la Categoría Primera A luego de obtener el título de la Primera B en 2010.",
+        estadio: "Alberto Grisales",
+        estrellas: "0",
+        categoria: "B",
+        pj:"8",
+        gf:"+8",
+        gc:"+7",
+        pts:"12",
+    },
+    {
+        nombre: "Deportivo Pereira",
+        urlimagen: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Deportivo_Pereira.png/168px-Deportivo_Pereira.png",
+        dt: "Jorge Luis Bernal",
+        jugadores: ["L. Hurtado", "J. Pérez", "L. Velázquez", "F. Rodríguez", "J. Valencia", "E. Toloza"],
+        resena: "Rionegro Águilas Doradas​ es un club de fútbol colombiano que se ubica en el municipio de Rionegro, Antioquia. Juega en la Categoría Primera A luego de obtener el título de la Primera B en 2010.",
+        estadio: "Alberto Grisales",
+        estrellas: "0",
+        categoria: "B",
+        pj:"8",
+        gf:"+8",
+        gc:"+7",
+        pts:"12",
+    },
+    {
+        nombre: "Real Cartagena",
+        urlimagen: "https://pbs.twimg.com/profile_images/819287065242255369/b3g_8e7y_400x400.jpg",
+        dt: "Jorge Luis Bernal",
+        jugadores: ["L. Hurtado", "J. Pérez", "L. Velázquez", "F. Rodríguez", "J. Valencia", "E. Toloza"],
+        resena: "Rionegro Águilas Doradas​ es un club de fútbol colombiano que se ubica en el municipio de Rionegro, Antioquia. Juega en la Categoría Primera A luego de obtener el título de la Primera B en 2010.",
+        estadio: "Alberto Grisales",
+        estrellas: "0",
+        categoria: "B",
+        pj:"8",
+        gf:"+8",
+        gc:"+7",
+        pts:"12",
+    },
+    {
+        nombre: "Valledupar FC",
+        urlimagen: "https://pbs.twimg.com/profile_images/859399310752010240/t8Lw8aTN_400x400.jpg",
+        dt: "Jorge Luis Bernal",
+        jugadores: ["L. Hurtado", "J. Pérez", "L. Velázquez", "F. Rodríguez", "J. Valencia", "E. Toloza"],
+        resena: "Rionegro Águilas Doradas​ es un club de fútbol colombiano que se ubica en el municipio de Rionegro, Antioquia. Juega en la Categoría Primera A luego de obtener el título de la Primera B en 2010.",
+        estadio: "Alberto Grisales",
+        estrellas: "0",
+        categoria: "B",
+        pj:"8",
+        gf:"+8",
+        gc:"+7",
+        pts:"12",
+    },
+    {
+        nombre: "Cortuluá",
+        urlimagen: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/Cortulu%C3%A1.jpg/160px-Cortulu%C3%A1.jpg",
+        dt: "Jorge Luis Bernal",
+        jugadores: ["L. Hurtado", "J. Pérez", "L. Velázquez", "F. Rodríguez", "J. Valencia", "E. Toloza"],
+        resena: "Rionegro Águilas Doradas​ es un club de fútbol colombiano que se ubica en el municipio de Rionegro, Antioquia. Juega en la Categoría Primera A luego de obtener el título de la Primera B en 2010.",
+        estadio: "Alberto Grisales",
+        estrellas: "0",
+        categoria: "B",
+        pj:"8",
+        gf:"+8",
+        gc:"+7",
+        pts:"12",
+    },
+    {
+        nombre: "Tigres FC",
+        urlimagen: "https://cdn.colombia.com/images/futbol/equipos/180x180/colombia/tigres-fc.jpg",
+        dt: "Jorge Luis Bernal",
+        jugadores: ["L. Hurtado", "J. Pérez", "L. Velázquez", "F. Rodríguez", "J. Valencia", "E. Toloza"],
+        resena: "Rionegro Águilas Doradas​ es un club de fútbol colombiano que se ubica en el municipio de Rionegro, Antioquia. Juega en la Categoría Primera A luego de obtener el título de la Primera B en 2010.",
+        estadio: "Alberto Grisales",
+        estrellas: "0",
+        categoria: "B",
+        pj:"8",
+        gf:"+8",
+        gc:"+7",
+        pts:"12",
+    },
+    {
+        nombre: "Universitario Popayán",
+        urlimagen: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7d/Universitario_de_Popay%C3%A1n.png/133px-Universitario_de_Popay%C3%A1n.png",
+        dt: "Jorge Luis Bernal",
+        jugadores: ["L. Hurtado", "J. Pérez", "L. Velázquez", "F. Rodríguez", "J. Valencia", "E. Toloza"],
+        resena: "Rionegro Águilas Doradas​ es un club de fútbol colombiano que se ubica en el municipio de Rionegro, Antioquia. Juega en la Categoría Primera A luego de obtener el título de la Primera B en 2010.",
+        estadio: "Alberto Grisales",
+        estrellas: "0",
+        categoria: "B",
+        pj:"8",
+        gf:"+8",
+        gc:"+7",
+        pts:"12",
+    },
+]
+
+// var selEquipos = document.getElementById("equipos");
+
+var nombre = document.getElementById("nombre");
+var imagenEquipo = document.getElementById("imgEquipo");
+var dt = document.getElementById("dt");
+var jugadores = document.getElementById("jugadores");
+var resena = document.getElementById("resena");
+var estadio = document.getElementById("estadio");
+var estrellas = document.getElementById("estrellas");
+var cont = document.getElementById("contenedor");
+var cont2 = document.getElementById("contenedor2");
+var equipos = "";
+var equipos2 = "";
+
+dataEquipos.forEach(function (item) { //renderizar equipos en html
+    if (item.categoria === "A") {
+        equipos += "<div class='gallery_product col-lg-3 col-md-3 col-sm-3 col-xs-6 filter hdpe'>" + "<div class='card'>" + 
+        "<h4 style='text-align: center'>" + item.nombre + "</h4>" + "<img src='" + item.urlimagen + "' class='img-responsive' width='250' height='250'>" 
+        + "<button type='button' class='btn btn-primary' data-toggle='modal' data-target='.bd-example-modal-lg'>Mas información</button>" + "</div>" + "</div>"
+        cont.innerHTML = equipos;
+    }
+});
+
+dataEquipos.forEach(function (item) { //renderizar equipos en html
+    if (item.categoria === "B") {
+        equipos2 += "<div class='gallery_product col-lg-3 col-md-3 col-sm-3 col-xs-6 filter hdpe'>" + "<div class='card'>" + 
+        "<h4 style='text-align: center'>" + item.nombre + "</h4>" + "<img src='" + item.urlimagen + "' class='img-responsive' width='250' height='250'>" 
+        + "<button type='button' class='btn btn-primary' data-toggle='modal' data-target='.bd-example-modal-lg'>Mas información</button>" + "</div>" + "</div>"
+        cont2.innerHTML = equipos2;
+    }
+});
+
+// selEquipos.onchange = function(){
+//     nombre.innerHTML = dataEquipos[this.selectedIndex].nombre;
+//     // imagenEquipo.setAttribute('src',dataEquipos[this.selectedIndex].urlimagen);
+//     dt.innerHTML = dataEquipos[this.selectedIndex].dt; 
+//     jugadores.innerHTML = dataEquipos[this.selectedIndex].jugadores; 
+//     resena.innerHTML = dataEquipos[this.selectedIndex].resena; 
+//     estadio.innerHTML = dataEquipos[this.selectedIndex].estadio; 
+//     estrellas.innerHTML = dataEquipos[this.selectedIndex].estrellas;    
+// }
